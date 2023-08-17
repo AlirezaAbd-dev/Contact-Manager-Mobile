2bd55897fdf0cedb91844300e34f0d2b0801b862
0AliReza0 <alireza.abedi9310@gmail.com>
2023-08-17 13:16:04 +0330

    FEATURED: token will be saved in async storage for authorization purposes...

--- screens/sign/Login.tsx
+++ screens/sign/Login.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import RegisterCard, { FormSchemaType } from '../../components/UI/RegisterCard';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import RegisterCard, { FormSchemaType } from '../../components/UI/RegisterCard';
 
 const Login = () => {
    async function onLoginHandler(formData: FormSchemaType) {
-      const response = await axios.post(
-         'https://contact-manager-ecru.vercel.app/api/login',
-         {
-            ...formData,
-         },
-      );
-      console.log(response);
+      console.log('login...');
+      try {
+         const response = await axios.post(
+            'https://contact-manager-ecru.vercel.app/api/login',
+            {
+               ...formData,
+            },
+         );
+         console.log(response);
+         console.log(response.headers['x-authentication-token']);
+         await AsyncStorage.setItem(
+            'token',
+            response.headers['x-authentication-token'],
+         );
+         console.log('Success...');
+      } catch (err) {
+         console.error(err);
+      }
    }
 
    return (
--- screens/sign/Signin.tsx
+++ screens/sign/Signin.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import RegisterCard, { FormSchemaType } from '../../components/UI/RegisterCard';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Signin = () => {
-   function onSigninHandler(formData: FormSchemaType) {
-      console.log(formData);
+   async function onSigninHandler(formData: FormSchemaType) {
+      console.log('signin...');
+      try {
+         const response = await axios.post(
+            'https://contact-manager-ecru.vercel.app/api/signIn',
+            {
+               ...formData,
+            },
+         );
+         console.log(response);
+         console.log(response.headers['x-authentication-token']);
+         await AsyncStorage.setItem(
+            'token',
+            response.headers['x-authentication-token'],
+         );
+         console.log('Success...');
+      } catch (err) {
+         console.error(err);
+      }
    }
    return (
       <RegisterCard
